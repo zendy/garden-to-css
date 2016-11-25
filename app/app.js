83ff1707b050f3e4186b9f732c3c46a9c5beed18
zendy <zendywongso@gmail.com>
2016-11-26 00:57:49 +1100

    Setting is working

--- app/app.js
+++ app/app.js
@@ -1,1 +1,22 @@
-console.log('ccc');
+const elButton = document.querySelectorAll('button')[0];
+
+const grabInput = () => {
+  const elTextArea = document.querySelectorAll('textarea')[0];
+  return elTextArea.value;
+};
+
+const transpileCss = (content) => {
+  let result = content.replace(/\[/g, '');
+  return result;
+};
+
+const showExport = (result) => {
+  const elTextArea = document.querySelectorAll('textarea')[1];
+  elTextArea.value = result;
+  return true;
+};
+
+elButton.addEventListener('click', (ev) => {
+  ev.preventDefault();
+  showExport(transpileCss(grabInput()));
+});
