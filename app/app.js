11122cfafc1406b201bc385bd99275bb8ab8faa0
zendy <zendywongso@gmail.com>
2016-11-29 10:27:22 +1100

    Add replace for constants

--- app/app.js
+++ app/app.js
@@ -14,7 +14,15 @@ const transpileCss = (content) => {
               .replace(/(\[)/g, '')
               .replace(/(\])/g, '')
               .replace(/(\s)+{/g, '\n{\n')
-              .replace(/}/g, '\n}');
+              .replace(/}/g, '\n}')
+              .replace('constants/success-green', '#8ed45a')
+              .replace('constants/default-font-family', 'Gotham, sans-serif')
+              .replace('constants/button-red', '#e75847')
+              .replace('constants/button-red-selected', '#bb382a')
+              .replace('constants/salon-info-background-grey', '#F2F2F4')
+              .replace('constants/light-grey-for-text', '#73767f')
+              .replace('constants/basic-border', '1px solid #d9dad8')
+              .replace('constants/error-red', '#e75847');
   return result;
 };
 
