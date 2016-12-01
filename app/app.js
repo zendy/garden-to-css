403dbbeb6f1c06bd671a8cc402a87a46aac4fe18
zendy <zendywongso@gmail.com>
2016-12-01 15:27:19 +1100

    Add replace terms

--- app/app.js
+++ app/app.js
@@ -8,21 +8,26 @@ const grabInput = () => {
 const transpileCss = (content) => {
   const result = content
               .replace(/:&:/g, '& ')
+              .replace(/(\[:)/g, '')
+              .replace(/(\[")/g, '')
               .replace(/(:)/g, '')
               .replace(/(\s)+"/g, ': ')
               .replace(/"/g, '')
-              .replace(/(\[)/g, '')
+              .replace(/\]\)/g, ']}')
               .replace(/(\])/g, '')
               .replace(/(\s)+{/g, '\n{\n')
               .replace(/}/g, '\n}')
-              .replace('constants/success-green', '#8ed45a')
-              .replace('constants/default-font-family', 'Gotham, sans-serif')
-              .replace('constants/button-red', '#e75847')
-              .replace('constants/button-red-selected', '#bb382a')
-              .replace('constants/salon-info-background-grey', '#F2F2F4')
-              .replace('constants/light-grey-for-text', '#73767f')
-              .replace('constants/basic-border', '1px solid #d9dad8')
-              .replace('constants/error-red', '#e75847');
+              .replace(/constants\/success-green/g, '#8ed45a')
+              .replace(/constants\/default-font-family/g, 'Gotham, sans-serif')
+              .replace(/constants\/button-red/g, '#e75847')
+              .replace(/constants\/button-red-selected/g, '#bb382a')
+              .replace(/constants\/salon-info-background-grey/g, '#F2F2F4')
+              .replace(/constants\/light-grey-for-text/g, '#73767f')
+              .replace(/constants\/basic-border/g, '1px solid #d9dad8')
+              .replace(/constants\/error-red/g, '#e75847')
+              .replace(/\(mq\/medium/g, '@media (--mq-medium){')
+              .replace(/\(mq\/large/g, '@media (--mq-large){')
+              .replace(/\(mq\/extra-large/g, '@media (--mq-extralarge){');
   return result;
 };
 
